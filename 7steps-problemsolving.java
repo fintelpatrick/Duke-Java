344ce8530595d401918f1621c32f5bcef9ea05d0
Patrick <fintelpatrick01@gmail.com>
2023-06-28 11:51:56 -0700

    Initial commit, starting repository and adding files already wrtitten

---
+++ 7steps-problemsolving.java
@@ -0,0 +1,38 @@
+The 7 steps to solving programming problems
+
+1: Work Example by hand;
+    -> make sure you understand the problem;
+        - need domain knowledge: if the problem is a math or physics problem,
+          make sure you understand the concept before trying to attack it;
+    -> try to work a smaller version of the problem by hand;
+        - if the problem is moving pictures in a picture, think about it first
+          as moving only, say, 4 pixels;
+2: Write down what you did;
+    -> Write down the exact steps of what was done;
+    -> just that instance: the exact steps to solve this little probelm, not
+       something more general;
+    -> Tricky: do without thinking;
+       - be consious of the things we do without thinking about them: the computer
+         does not have this ability;
+3: Find patterns;
+    -> Algorithm for any instance;
+    -> Find parterns:
+       - Repitition, conditions, values: The things we do over and over;
+       - The point where things loop back;
+    -> Difficulties?;
+       - Try step 1 + 2 again;
+       - Different inputs;
+4: Check by Hand:
+    -> Incorrect pattern?;
+       - Find now: Check with different inputs;
+5: Translate to Code:
+    -> Programming language;
+6: Run test cases on code:
+    -> Execute program;
+    -> Check answer: Must know the right answer before time;
+7: Debug Failed Test Cases;
+    -> Test failed? Debug;
+       - use scientific method;
+       - understand the problem and fix;
+    -> Algorithmic problem?: return to step 3;
+    -> Implementation problem?: return to step 5;
---
+++ hello/HelloWorld2.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of HelloWorld2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import edu.duke.FileResource;
+public class HelloWorld2 {
+    public void runHello() {
+        FileResource f;
+        f = new FileResource("file.txt");
+        for(String line : f.lines()){
+            System.out.println(line);
+        }
+    }
+    public static void main(String[] args){
+        HelloWorld2 hw = new HelloWorld2();
+        hw.runHello();
+    }
+
+}
